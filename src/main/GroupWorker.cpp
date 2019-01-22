6be9da27ed7f0cad7db94e402f9ecc1a502fd08d
project-alchemist <kai.rothauge@berkeley.edu>
2019-01-21 19:32:24 -0800

    Debugging Linux dl

--- src/main/GroupWorker.cpp
+++ src/main/GroupWorker.cpp
@@ -312,7 +312,7 @@ int GroupWorker::load_library()
 
 	log->info("Loading library {} located at {}", library_name, library_path);
 
-	void * lib = dlopen(library_path.c_str(), RTLD_LAZY);
+	void * lib = dlopen(library_path.c_str(), RTLD_LAZY | RTLD_GLOBAL);
 	log->info("L 1");
 	const char * dlopen_error = dlerror();
 	log->info("L 2");
