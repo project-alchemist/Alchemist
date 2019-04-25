86e4734345e6a2c4a25453a2d245d188ed7ce2b3
kai-rothauge <kai.rothauge@berkeley.edu>
2019-04-24 18:36:16 -0700

    Installation file for Cori

--- setup/Cori/install.sh
+++ setup/Cori/install.sh
@@ -13,9 +13,15 @@
 #   bash $ALCHEMIST_PATH/setup/Cori/install.sh
 #
 
+INSTALL_ELEMENTAL=1
+INSTALL_ARPACK=1
+INSTALL_EIGEN=1
+INSTALL_SPDLOG=1
+INSTALL_ASIO=1
+
 export ELEMENTAL_PATH=$SCRATCH/lib/Elemental
-export SPDLOG_PATH=$SCRATCH/lib/SPDLog	
-export EIGEN3_PATH=$SCRATCH/lib/Eigen3
+export SPDLOG_PATH=$SCRATCH/lib/spdlog	
+export EIGEN3_PATH=$SCRATCH/lib/Eigen
 export ARPACK_PATH=$SCRATCH/lib/ARPACK
 export ASIO_PATH=$SCRATCH/lib/asio
 
@@ -25,8 +31,14 @@ export CXX="CC"
 export FC="ftn"
 export TEMP_DIR=$SCRATCH/lib/temp
 
+MAKE_THREADS=16
 
 mkdir -p $TEMP_DIR
+mkdir -p $ELEMENTAL_PATH
+mkdir -p $SPDLOG_PATH
+mkdir -p $EIGEN3_PATH
+mkdir -p $ARPACK_PATH
+mkdir -p $ASIO_PATH
 
 
 # Check that the cmake toolchain file is where we expect
@@ -48,66 +60,92 @@ module load hdf5-parallel
 
 
 # Install Elemental
-cd $TEMP_DIR && \
-    git clone git://github.com/elemental/Elemental.git && \
-    cd Elemental && \
-    git checkout 0.87 && \
-    mkdir build && \
-    cd build && \
-    CC=gcc-7 CXX=g++-7 FC=gfortran-7 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$ELEMENTAL_PATH .. && \
-    nice make -j4  && \
-    make install  && \
-    cd $TEMP_DIR && \
+if [ "$INSTALL_ELEMENTAL" = 1 ]; then
+	echo "Installing Elemental"
+	echo "--------------------"
+	cd $TEMP_DIR
+	git clone git://github.com/elemental/Elemental.git
+	cd Elemental
+    git checkout 0.87
+    mkdir -p build
+    cd build
+    CC=gcc-7 CXX=g++-7 FC=gfortran-7 cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$ELEMENTAL_PATH ..
+    nice make -j"MAKE_THREADS"
+    make install
+    cd $TEMP_DIR
     rm -rf Elemental
+fi
 
 # Install ARPACK
-cd $TEMP_DIR && \
-    git clone https://github.com/opencollab/arpack-ng && \
-    cd arpack-ng && \
-    git checkout 3.5.0 && \
-    mkdir build && \
-    cd build && \
-    cmake -DMPI=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=${ARPACK_PATH} .. && \
-    nice make -j4 && \
-    make install && \
-    cd $TEMP_DIR && \
+if [ "$INSTALL_ARPACK" = 1 ]; then
+	echo "Installing Arpack-ng"
+	echo "--------------------"
+	cd $TEMP_DIR
+    git clone https://github.com/opencollab/arpack-ng
+    cd arpack-ng
+    git checkout 3.5.0
+    mkdir build
+    cd build
+    cmake -DMPI=ON -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=${ARPACK_PATH} ..
+    nice make -j"MAKE_THREADS"
+    make install
+    cd $TEMP_DIR
     rm -rf arrack-ng
 
-# Install ARPACKPP
-cd $TEMP_DIR && \
-    git clone https://github.com/m-reuter/arpackpp && \
-    cd arpackpp && \
-    git checkout 88085d99c7cd64f71830dde3855d73673a5e872b && \
-    mkdir build && \
-    cd build && \
-    cmake -DCMAKE_INSTALL_PREFIX=${ARPACK_PATH} .. && \
-    make install && \
-    cd $TEMP_DIR && \
+	echo "Installing Arpackpp"
+	echo "-------------------"
+    cd $TEMP_DIR
+    git clone https://github.com/m-reuter/arpackpp
+    cd arpackpp
+    git checkout 88085d99c7cd64f71830dde3855d73673a5e872b
+    mkdir build
+    cd build
+    cmake -DCMAKE_INSTALL_PREFIX=${ARPACK_PATH} ..
+    make install
+    cd $TEMP_DIR
     rm -rf arpackpp
+fi
 
-# Install Eigen3
-cd $TEMP_DIR && \
-    curl -L http://bitbucket.org/eigen/eigen/get/3.3.4.tar.bz2 | tar xvfj - && \
-    cd eigen-eigen-5a0156e40feb && \
-    mkdir build && \
-    cd build && \
-    cmake -DCMAKE_INSTALL_PREFIX=${EIGEN3_PATH} .. && \
-    nice make -j4 && \
-    make install && \
-    cd $TEMP_DIR && \
+# Install Eigen
+if [ "$INSTALL_EIGEN" = 1 ]; then
+	echo "Installing Eigen"
+	echo "----------------"
+	cd $TEMP_DIR
+    curl -L http://bitbucket.org/eigen/eigen/get/3.3.4.tar.bz2 | tar xvfj -
+    cd eigen-eigen-5a0156e40feb
+    mkdir build
+    cd build
+    cmake -DCMAKE_INSTALL_PREFIX=${EIGEN3_PATH} ..
+    nice make -j"MAKE_THREADS"
+    make install
+    cd $TEMP_DIR
     rm -rf eigen-eigen-5a0156e40feb
+fi
 
-# Install SPDlog
-cd $TEMP_DIR && \
-    git clone https://github.com/gabime/spdlog.git && \
-    cd spdlog && \
-    mkdir $SPDLOG_PATH && cp -r include/ $SPDLOG_PATH/include/ && \
-    cd $TEMP_DIR && rm -rf spdlog
+# Install spdlog
+if [ "$INSTALL_SPDLOG" = 1 ]; then
+	echo "Installing spdlog"
+	echo "----------------"
+	cd $TEMP_DIR
+    git clone https://github.com/gabime/spdlog.git
+    cd spdlog
+    mkdir -p $SPDLOG_PATH
+    cp -r include/ $SPDLOG_PATH/include/
+    cd $TEMP_DIR
+    rm -rf spdlog
+fi
 
 # Install asio
-cd $TEMP_DIR && \
-    wget -O asio-1.12.1.zip https://sourceforge.net/projects/asio/files/asio/1.12.1%20%28Stable%29/asio-1.12.1.zip/download && \
-    unzip asio-1.12.1.zip && \
-    cd asio-1.12.1 && ./configure --prefix=$ASIO_PATH && make install -j4 && \
-    cd $TEMP_DIR && rm -rf asio-1.12.1 asio-1.12.1.zip
+if [ "$INSTALL_ASIO" = 1 ]; then
+	echo "Installing asio"
+	echo "---------------"
+	cd $TEMP_DIR
+    wget -O asio-1.12.1.zip https://sourceforge.net/projects/asio/files/asio/1.12.1%20%28Stable%29/asio-1.12.1.zip/download
+    unzip asio-1.12.1.zip
+    cd asio-1.12.1
+    ./configure --prefix=$ASIO_PATH --without-boost
+    make install -j"MAKE_THREADS"
+    cd $TEMP_DIR
+    rm -rf asio-1.12.1 asio-1.12.1.zip
+fi
 
